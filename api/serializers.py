f8061d392758e0505c99862f3f1cfe38424d8da7
pavelusov79 <pavel.usov79@gmail.com>
2023-01-30 15:42:52 +1000

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -13,14 +13,13 @@ class CountrySerializer(serializers.ModelSerializer):
 
 
 class CountryRegionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CountryRegion
         fields = ('country', 'region_name')
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Product
         fields = ('product_name', 'product_grade', 'product_year', 'country', 'region')
